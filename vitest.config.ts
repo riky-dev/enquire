fb5309088bc7df340e2e5095efda2169ceb792e5
riky-dev <58308241+riky-dev@users.noreply.github.com>
2026-06-07 18:48:28 +0200

    feat: add e2e tests with playwright

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig, configDefaults } from "vitest/config";
 import path from "path";
 
 export default defineConfig({
   test: {
     environment: "node",
+    exclude: [...configDefaults.exclude, "e2e/**"],
   },
   resolve: {
     alias: {
